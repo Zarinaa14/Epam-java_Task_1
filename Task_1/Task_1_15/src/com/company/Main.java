f25b8bcc635c512ca3f950067828765cf3b889f5
Zarinaa14 <32223597+Zarinaa14@users.noreply.github.com>
2019-09-12 16:35:50 +0300

    aa
    aa

--- Task_1/Task_1_15/src/com/company/Main.java
+++ Task_1/Task_1_15/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // 4. Найти сумму квадратов первых 200 чисел.
-        int k=0;
+        long k=0;
         for (int i=1; i<=200; i++ ){
             k+=i*i;
         }
