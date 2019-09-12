4fd0e728243fd6845b63183ddb8f4b53a8d47489
Zaryna L <smal_david@mail.ru>
2019-09-12 19:53:29 +0300

    df

--- Task_1/Linear programs/Task_1_3/src/com/company/Main.java
+++ Task_1/Linear programs/Task_1_3/src/com/company/Main.java
@@ -9,16 +9,14 @@ public class Main {
         // (𝑠𝑖𝑛𝑥 + 𝑐𝑜𝑠𝑦)/ (𝑐𝑜𝑠𝑥 − 𝑠𝑖𝑛𝑦) ∗ 𝑡𝑔 𝑥𝑦
         Scanner in = new Scanner(System.in);
         double x, y, z;
-        System.out.println("Enter value of x [-1,1]: ");
+        System.out.println("Enter value of x: ");
         x = in.nextDouble();
         //
-        System.out.println("Enter value of y [-1,1]: ");
+        System.out.println("Enter value of y: ");
         y = in.nextDouble();
-        //
-        if (x >= -1 && 1 >= x && y >= -1 && 1 >= y ) {
             z = (Math.sin(x)+Math.cos(y))/(Math.cos(x) - Math.sin(y)) * Math.tan(x*y);
             System.out.printf("The Result is = %.2f", z);
-        } else  System.out.println("Введите х в промежутке[-1,1]");
+
 
         //System.out.printf("The Result is = %.2f", z);
         in.close();
